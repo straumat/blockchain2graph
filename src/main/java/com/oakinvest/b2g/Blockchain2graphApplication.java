c5e640b9dabc5418134c9c5b318b840bf31fceca
straumat <stephane.traumat@gmail.com>
2016-08-24 22:36:38 +0200

    Setting up Spring Boot

---
+++ src/main/java/com/oakinvest/b2g/Blockchain2graphApplication.java
@@ -0,0 +1,12 @@
+package com.oakinvest.b2g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blockchain2graphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blockchain2graphApplication.class, args);
+	}
+}
