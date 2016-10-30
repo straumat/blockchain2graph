97362d87a5d27f4289b5ac7dc1c026b1619325d5
straumat <stephane.traumat@gmail.com>
2016-10-30 18:04:52 +0100

    Correcting checkstyle failure after commit

---
+++ src/main/java/com/oakinvest/b2g/service/bitcoin/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Services for bitcoin.
+ * Created by straumat on 30/10/16.
+ */
+package com.oakinvest.b2g.service.bitcoin;
---
+++ src/main/java/com/oakinvest/b2g/util/bitcoin/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Util for bitcoin.
+ * Created by straumat on 30/10/16.
+ */
+package com.oakinvest.b2g.util.bitcoin;
