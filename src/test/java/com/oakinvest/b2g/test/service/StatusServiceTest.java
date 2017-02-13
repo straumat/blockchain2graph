f14a9182e005c4c9a0918922b3b3d5ad8707a1bd
straumat <stephane.traumat@gmail.com>
2017-02-13 22:33:41 +0100

    Adding the mean execution time.

--- src/test/java/com/oakinvest/b2g/test/service/StatusServiceTest.java
+++ src/test/java/com/oakinvest/b2g/test/service/StatusServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -19,6 +20,7 @@ import static org.junit.Assert.assertTrue;
 @ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class StatusServiceTest {
 
 	/**
