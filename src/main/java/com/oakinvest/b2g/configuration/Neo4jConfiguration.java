ff2e129ca14265dd6872487618abe6a01a86a6fc
straumat <stephane.traumat@gmail.com>
2017-03-02 22:50:31 +0100

    Making neo4Jconfiguration outside of Application

---
+++ src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
@@ -0,0 +1,13 @@
+package com.oakinvest.b2g.configuration;
+
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+
+/**
+ * Neo4j configuration.
+ * Created by straumat on 02/03/17.
+ */
+@EnableNeo4jRepositories
+public class Neo4jConfiguration {
+
+
+}
