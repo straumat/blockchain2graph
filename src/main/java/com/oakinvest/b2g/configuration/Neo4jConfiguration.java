e665d1d2137d0a836def3f7c981df520f4555e68
straumat <stephane.traumat@gmail.com>
2018-01-12 16:48:17 +0100

    Index automatic creation

--- src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
+++ src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
@@ -3,7 +3,6 @@ package com.oakinvest.b2g.configuration;
 import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
@@ -23,9 +22,10 @@ public class Neo4jConfiguration {
 
     /**
      * Constructor - Create constraints and indexes.
+     *
+     * @param sessionFactory session factory
      */
-    @Autowired
-    public Neo4jConfiguration(SessionFactory sessionFactory) {
+    public Neo4jConfiguration(final SessionFactory sessionFactory) {
         try {
             // Session.
             Session session = sessionFactory.openSession();
