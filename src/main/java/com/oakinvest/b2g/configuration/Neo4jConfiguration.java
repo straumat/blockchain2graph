cc6f8ba3e745592194b31fcb927a7a5cb21f2016
straumat <stephane.traumat@gmail.com>
2018-01-27 22:25:54 +0100

    Removing index creation

--- src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
+++ src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
@@ -1,14 +1,9 @@
 package com.oakinvest.b2g.configuration;
 
-import org.neo4j.ogm.session.Session;
-import org.neo4j.ogm.session.SessionFactory;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.Collections;
 
 /**
  * Neo4j configuration class.
@@ -19,26 +14,5 @@ import java.util.Collections;
 @EnableNeo4jRepositories(basePackages = "com.oakinvest.b2g")
 @EntityScan(basePackages = "com.oakinvest.b2g.domain")
 public class Neo4jConfiguration {
-
-    /**
-     * Constructor - Create constraints and indexes.
-     *
-     * @param sessionFactory session factory
-     */
-    public Neo4jConfiguration(final SessionFactory sessionFactory) {
-        try {
-            // Session.
-            Session session = sessionFactory.openSession();
-            // Constraints.
-            session.query("CREATE CONSTRAINT ON (n:BitcoinAddress) ASSERT n.address IS UNIQUE", Collections.emptyMap());
-            session.query("CREATE CONSTRAINT ON (n:BitcoinBlock) ASSERT n.hash IS UNIQUE", Collections.emptyMap());
-            session.query("CREATE CONSTRAINT ON (n:BitcoinTransaction) ASSERT n.txid IS UNIQUE", Collections.emptyMap());
-            // Indexes.
-            session.query("CREATE INDEX ON :BitcoinBlock(height)", Collections.emptyMap());
-            session.query("CREATE INDEX ON :BitcoinTransactionOutput(txid, n)", Collections.emptyMap());
-        } catch (Exception e) {
-            LoggerFactory.getLogger(Neo4jConfiguration.class).error("Error creating constraints & indexes : " + e.getMessage(), e);
-        }
-    }
 
 }
