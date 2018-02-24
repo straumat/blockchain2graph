6440da964839fea9b561a5a1b31f9a2a721461db
straumat <stephane.traumat@gmail.com>
2018-02-25 00:38:32 +0100

    Back to normal (it was one day test instead of one hour test as i thought)

--- src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
+++ src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
@@ -1,16 +1,44 @@
 package com.oakinvest.b2g.configuration;
 
+import org.neo4j.ogm.session.Session;
+import org.neo4j.ogm.session.SessionFactory;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
 
 /**
  * Neo4j configuration class.
  * Created by straumat on 02/03/17.
  */
 @Configuration
+@EnableTransactionManagement
 @EnableNeo4jRepositories(basePackages = "com.oakinvest.b2g")
 @EntityScan(basePackages = "com.oakinvest.b2g.domain")
 public class Neo4jConfiguration {
+
+    /**
+     * Create constraints and indexes.
+     */
+    @PostConstruct
+    public final void createConstraintsAndIndexes() {
+        try {
+            // Session.
+            Session session = new SessionFactory("com.oakinvest.b2g").openSession();
+            // Constraints.
+            session.query("CREATE CONSTRAINT ON (n:BitcoinAddress) ASSERT n.address IS UNIQUE", Collections.emptyMap());
+            // Indexes.
+            session.query("CREATE INDEX ON :BitcoinBlock(height)", Collections.emptyMap());
+            session.query("CREATE INDEX ON :BitcoinBlock(hash)", Collections.emptyMap());
+            session.query("CREATE INDEX ON :BitcoinTransaction(txid)", Collections.emptyMap());
+            session.query("CREATE INDEX ON :BitcoinTransactionOutput(txid, n)", Collections.emptyMap());
+        } catch (Exception e) {
+            LoggerFactory.getLogger(Neo4jConfiguration.class).error("Error creating constraints & indexes : " + e.getMessage(), e);
+        }
+    }
 
 }
