7c04be7d1eebc7ce3dcf169ac95b3ba269ffff29
straumat <stephane.traumat@gmail.com>
2018-02-24 11:05:13 +0100

    Removing transaction management

--- src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
+++ src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
@@ -3,14 +3,12 @@ package com.oakinvest.b2g.configuration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Neo4j configuration class.
  * Created by straumat on 02/03/17.
  */
 @Configuration
-@EnableTransactionManagement
 @EnableNeo4jRepositories(basePackages = "com.oakinvest.b2g")
 @EntityScan(basePackages = "com.oakinvest.b2g.domain")
 public class Neo4jConfiguration {
