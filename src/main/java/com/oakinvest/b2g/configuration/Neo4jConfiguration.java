c8490f1bb987d648fa58343456afab1f13454f1c
straumat <stephane.traumat@gmail.com>
2018-03-15 11:01:04 +0100

    Automatic index creation ( closes #178 )

--- src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
+++ src/main/java/com/oakinvest/b2g/configuration/Neo4jConfiguration.java
@@ -1,9 +1,16 @@
 package com.oakinvest.b2g.configuration;
 
+import org.neo4j.ogm.session.Session;
+import org.neo4j.ogm.session.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
 
 /**
  * Neo4j configuration class.
@@ -14,5 +21,41 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableNeo4jRepositories(basePackages = "com.oakinvest.b2g")
 @EntityScan(basePackages = "com.oakinvest.b2g.domain")
 public class Neo4jConfiguration {
+
+    /**
+     * Session factory.
+     */
+    private final SessionFactory sessionFactory;
+
+    /**
+     * Constructor.
+     *
+     * @param newSessionFactory session factory
+     */
+    public Neo4jConfiguration(final SessionFactory newSessionFactory) {
+        this.sessionFactory = newSessionFactory;
+    }
+
+    /**
+     * Creates constraints and indexes.
+     */
+    @PostConstruct
+    public final void createConstraintsAndIndexes() {
+        Logger log = LoggerFactory.getLogger(Neo4jConfiguration.class);
+
+        try {
+            log.info("Constraints and indexes creation");
+            // Session.
+            Session session = sessionFactory.openSession();
+            // Constraints.
+            session.query("CREATE CONSTRAINT ON (n:BitcoinAddress) ASSERT n.address IS UNIQUE", Collections.emptyMap());
+            session.query("CREATE CONSTRAINT ON (n:BitcoinTransaction) ASSERT n.txid IS UNIQUE;", Collections.emptyMap());
+            // Indexes.
+            session.query("CREATE INDEX ON :BitcoinBlock(height)", Collections.emptyMap());
+            session.query("CREATE INDEX ON :BitcoinTransactionOutput(txid, n)", Collections.emptyMap());
+        } catch (Exception e) {
+            log.error("Error creating constraints & indexes : " + e.getMessage(), e);
+        }
+    }
 
 }
